0f57d73fef2443bc0bf7b9c4a384f64fa5ea610b
Jim Safley <jimsafley@gmail.com>
2015-12-08 19:27:35 -0500

    Hide sideload ingester when no files in directory

--- Module.php
+++ Module.php
@@ -1,13 +1,29 @@
 <?php
 namespace FileSideload;
 
+use Omeka\Event\Event as OmekaEvent;
 use Omeka\Module\AbstractModule;
+use Zend\EventManager\Event;
+use Zend\EventManager\SharedEventManagerInterface;
 
 class Module extends AbstractModule
 {
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            'Omeka\Media\Ingester\Manager',
+            OmekaEvent::SERVICE_REGISTERED_NAMES,
+            function (Event $event) {
+                $names = $event->getParam('registered_names');
+                $names[] = 'foobar';
+                $event->setParam('registered_names', $names);
+            }
+        );
     }
 }
 
