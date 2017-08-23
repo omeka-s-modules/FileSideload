4badee2384f69008456da4b22f601e5bd2f04b67
patrickmj <patrickmjchnm@gmail.com>
2017-08-23 15:36:24 -0400

    i18n

--- config/module.config.php
+++ config/module.config.php
@@ -5,6 +5,16 @@ return [
             'sideload' => 'FileSideload\Service\MediaIngesterSideloadFactory',
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => OMEKA_PATH . '/modules/FileSideload/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'csv_import_media_ingester_adapter' => [
         'sideload'   => 'FileSideload\CSVImport\SideloadMediaIngesterAdapter',
     ],
